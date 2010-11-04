26e6921f9b5bf0d51ffd25376c332a619323d416
Nick Hansen <nitronick600@gmail.com>
2010-11-04 00:49:28 -0600

    First Commit

---
+++ src/com/uid/DroidDoesMusic/DroidDoesMusic.java
@@ -0,0 +1,13 @@
+package com.uid.DroidDoesMusic;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DroidDoesMusic extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

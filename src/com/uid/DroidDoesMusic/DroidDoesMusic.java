b43f65bab88ffca674eb807987041e108b4c92bd
Nick Hansen <nitronick600@gmail.com>
2010-11-04 22:19:42 -0600

    Some basic skeleton code

--- src/com/uid/DroidDoesMusic/DroidDoesMusic.java
+++ src/com/uid/DroidDoesMusic/DroidDoesMusic.java
@@ -2,12 +2,25 @@ package com.uid.DroidDoesMusic;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class DroidDoesMusic extends Activity {
     /** Called when the activity is first created. */
-    @Override
+	private ListView lv;
+	
+	private String[] lvItems = {"Lil John", "Lil Wayne", "Yount Jeezy", "Chicago" };             
+	
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.music_list);
+        lv=(ListView)findViewById(R.id.ListView01);
+        Log.d("TESSST", this.toString());
+
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1 , lvItems));
     }
 }
+

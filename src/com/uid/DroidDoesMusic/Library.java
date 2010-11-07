2942cb3d60bdcbd23b56ed67f2b416ec5ccf28ad
Michael Rose <devnull@localhost>
2010-11-07 00:28:52 -0600

    SDcard Intent

--- src/com/uid/DroidDoesMusic/Library.java
+++ src/com/uid/DroidDoesMusic/Library.java
@@ -1,6 +1,10 @@
 package com.uid.DroidDoesMusic;
 
 import android.app.ListActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -21,12 +25,42 @@ public class Library extends ListActivity {
         setContentView(R.layout.library);
         
         getArtists();
+        
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
+        filter.addAction(Intent.ACTION_MEDIA_REMOVED);
+        registerReceiver(onSdMounted, filter);
 
-        if (!isSdPresent()) {
-        	TextView tv = (TextView)findViewById(R.id.empty);
-        	tv.setText(getResources().getString(R.string.no_sd_card));
-        }
+        sdCheck();
     }
+	
+	@Override
+	public void onResume() {
+        Log.d(TAG, getClass().getSimpleName() + ": onResume");
+		super.onResume();
+		
+		IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
+        filter.addAction(Intent.ACTION_MEDIA_REMOVED);
+        registerReceiver(onSdMounted, filter);
+	}
+	
+	@Override
+	public void onPause() {
+        Log.d(TAG, getClass().getSimpleName() + ": onPause");
+		super.onPause();
+		
+		unregisterReceiver(onSdMounted);
+	}
+	
+	public void sdCheck() {
+        if (!isSdPresent()) {
+        	TextView tv = (TextView)findViewById(android.R.id.empty);
+        	tv.setText(getResources().getString(R.string.no_sd_card));
+        } else {
+        	getArtists();
+        }
+	}
 	
 	public static boolean isSdPresent() {
 	    return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
@@ -44,10 +78,18 @@ public class Library extends ListActivity {
         // Activity-managed cursor to get sorted list of artists
         Cursor cur = managedQuery(Artists, projection, null, null, Audio.ArtistColumns.ARTIST + " ASC");
         
-        // SimpleCursorAdatper maps the cursor columns to simplelistitems
+        // SimpleCursorAdapter maps the cursor columns to simplelistitems
         SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cur, displayColumns, display);
         
         // Set adapter of listview to the SimpleCursorAdapter 
         setListAdapter(mAdapter);		
 	}
+	
+	BroadcastReceiver onSdMounted = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context content, Intent intent) {
+	        Log.d(TAG, getClass().getSimpleName() + ": onReceive: " + intent.getData());
+	        sdCheck();
+		}
+	};
 }

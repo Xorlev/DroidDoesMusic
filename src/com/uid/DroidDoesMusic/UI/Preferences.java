bca52e3f1f49c64edcd42c1ff6d2da2c5d8877f5
Xorlev <elementation@gmail.com>
2010-11-21 01:12:43 -0700

    Preferences disables Last.FM if the app is not installed

--- src/com/uid/DroidDoesMusic/UI/Preferences.java
+++ src/com/uid/DroidDoesMusic/UI/Preferences.java
@@ -1,10 +1,17 @@
 package com.uid.DroidDoesMusic.UI;
 
-import com.uid.DroidDoesMusic.R;
+import java.util.List;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.util.Log;
+
+import com.uid.DroidDoesMusic.R;
 
 public class Preferences extends PreferenceActivity {
 	protected static final String TAG = "DroidDoesMusic";
@@ -16,5 +23,26 @@ public class Preferences extends PreferenceActivity {
 		super.onCreate(savedInstanceState);
 		
 		addPreferencesFromResource(R.xml.settings);
+		
+		checkLastFM();
+	}
+	
+	public void checkLastFM() {
+		final boolean lastFMAvailable = isIntentAvailable(this, "fm.last.android.metachanged");
+		
+		if (!lastFMAvailable) {
+			Preference lastfm_scrobble = findPreference("lastfm_scrobble");
+			lastfm_scrobble.setEnabled(false);
+			lastfm_scrobble.setSummary(lastfm_scrobble.getSummary() + getResources().getString(R.string.lastfm_scrobble_app_required));
+		}
+	}
+	
+	public static boolean isIntentAvailable(Context context, String action) {
+	    final PackageManager packageManager = context.getPackageManager();
+	    final Intent intent = new Intent(action);
+	    List<ResolveInfo> list =
+	            packageManager.queryIntentActivities(intent,
+	                    PackageManager.MATCH_DEFAULT_ONLY);
+	    return list.size() > 0;
 	}
 }

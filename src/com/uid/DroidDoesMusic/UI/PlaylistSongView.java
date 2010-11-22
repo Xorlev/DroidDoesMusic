c03d0b4df6e419e2b0f36dce2134bcbe14e4d01d
Joe Zeimen <jzeimen@gmail.com>
2010-11-21 21:30:36 -0700

    Added nextSong and Current song to the PlaylistManager. Needs tested by service and next button. Still bringing up 2 screens when clicking on a playlist. Needs fixed

--- src/com/uid/DroidDoesMusic/UI/PlaylistSongView.java
+++ src/com/uid/DroidDoesMusic/UI/PlaylistSongView.java
@@ -1,23 +1,13 @@
 package com.uid.DroidDoesMusic.UI;
 
 import android.app.ListActivity;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.provider.MediaStore.Audio;
-import android.provider.MediaStore.Audio.Playlists.Members;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
 
-import com.uid.DroidDoesMusic.R;
 import com.uid.DroidDoesMusic.util.PlaylistManager;
 
 /**
@@ -33,17 +23,17 @@ public class PlaylistSongView extends ListActivity {
 	Uri extContentPlaylists = MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
 	private String playlistName = new String();
 	private int playlistId;
-	
+	private PlaylistManager mPlaylistManager;
     /** Called when the activity is first created. */	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, getClass().getSimpleName() + ": onCreate");
         super.onCreate(savedInstanceState);
-        
+        mPlaylistManager = PlaylistManager.getInstance(this);
         try {
 	        playlistName = getIntent().getExtras().getString(INTENT_ITEM_PLAYLIST_NAME);
 	        playlistId = getIntent().getExtras().getInt(INTENT_ITEM_PLAYLIST_ID);
-	        setTitle(playlistName);
+	       // setTitle(playlistName);
 	        
 	        if (playlistName == null) {
 	        	playlistName = "";
@@ -60,11 +50,11 @@ public class PlaylistSongView extends ListActivity {
 	public void onListItemClick(ListView l, View v, int position, long id) {
 		Log.d(TAG, getClass().getSimpleName() + ": onListItemClick: (" + id + ")");
 		super.onListItemClick(l, v, position, id);
+		mPlaylistManager.setSelectedPlaylist(playlistId);
+		mPlaylistManager.setPosition(position);
 
 	}
-	
-
-	
+		
 	
 }
 
--- src/com/uid/DroidDoesMusic/util/PlaylistManager.java
+++ src/com/uid/DroidDoesMusic/util/PlaylistManager.java
@@ -1,9 +1,14 @@
 package com.uid.DroidDoesMusic.util;
 
+import com.uid.DroidDoesMusic.player.Player;
+
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.ServiceConnection;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.IBinder;
 import android.provider.MediaStore;
 import android.provider.MediaStore.Audio;
 import android.provider.MediaStore.Audio.Playlists.Members;
@@ -15,12 +20,26 @@ public class PlaylistManager {
 	static private PlaylistManager instance;
 	Context context;
 	ContentResolver cr;
+	private static final String TAG = new String("DroidDoesMusic");
+	private static Player mPlayer;
+	private static boolean isPlayerBound;
 	private static Cursor mCur;
+	private static int mPosition;
+	private static int mCurrentPlaylist;
+	private static Cursor mCurrentPlaylistMembers;
+	private static final String [] STAR = {"*"};
+
+	//Positions in the next song string array
+	public static final int ARTIST=0;
+	public static final int ALBUM=1;
+	public static final int TITLE=2;
+	public static final int DATAPATH=3;
+
 	private PlaylistManager(Context context){
-		
+
 		this.context = context;
 		this.cr = context.getContentResolver();
-		
+
 	}
 	public static PlaylistManager getInstance(Context context){
 		if(instance == null){
@@ -28,12 +47,12 @@ public class PlaylistManager {
 		} else return instance;
 	}
 	public SimpleCursorAdapter listPlaylists(){
-   
-        String[] projection = {Audio.Playlists._ID,Audio.Playlists.NAME};
-        String[] displayColumns = {Audio.Playlists.NAME};
-        int layout = android.R.layout.simple_list_item_1;
-        int[] display = new int[] { android.R.id.text1};
-        mCur = cr.query(android.provider.MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, projection, null, null, Audio.Playlists.DEFAULT_SORT_ORDER);
+
+		String[] projection = {Audio.Playlists._ID,Audio.Playlists.NAME};
+		String[] displayColumns = {Audio.Playlists.NAME};
+		int layout = android.R.layout.simple_list_item_1;
+		int[] display = new int[] { android.R.id.text1};
+		mCur = cr.query(android.provider.MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI, projection, null, null, Audio.Playlists.DEFAULT_SORT_ORDER);
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(context,layout,mCur,displayColumns,display);
 		Log.d("DroidDoesMusic","Here I am:   "+android.provider.MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI.toString());
 		return adapter;
@@ -41,14 +60,83 @@ public class PlaylistManager {
 	public ListAdapter listSongs( int selection){
 		mCur.moveToPosition(selection);
 		ListAdapter adapter;
-        String[] projection = {Audio.AudioColumns.TITLE,Audio.AudioColumns._ID};
-        String[] displayColumns = {Audio.AudioColumns.TITLE};
-        int layout = android.R.layout.simple_list_item_1;
-        int[] display = new int[] { android.R.id.text1};
-        int id= mCur.getInt(0);
-        Uri membersUri = MediaStore.Audio.Playlists.Members.getContentUri("external", id);
-        mCur = cr.query(membersUri, projection, null, null, Audio.Media.DEFAULT_SORT_ORDER);
+		String[] displayColumns = {Audio.AudioColumns.TITLE};
+		int layout = android.R.layout.simple_list_item_1;
+		int[] display = new int[] { android.R.id.text1};
+		int id= mCur.getInt(0);
+		Uri membersUri = MediaStore.Audio.Playlists.Members.getContentUri("external", id);
+		mCur = cr.query(membersUri, STAR, null, null, Audio.Media.DEFAULT_SORT_ORDER);
 		adapter = new SimpleCursorAdapter(context,layout,mCur,displayColumns,display);
 		return adapter;
 	}
+
+	/**
+	 * Returns a string array for info on the next song.
+	 * use PlaylistManager.ARTIST, PlaylistManager.ALBUM
+	 * PlaylistManager.TITLE, PlaylistManager.DATAPATH for the
+	 * indexes on the string array.
+	 * Returns null if no next song.
+	 * 
+	 * @return
+	 */
+	public String [] nextSong(){
+		Uri membersUri = MediaStore.Audio.Playlists.Members.getContentUri("external", mCurrentPlaylist);
+		Cursor currentSongQuery = cr.query(membersUri, STAR, null, null, Audio.Media.DEFAULT_SORT_ORDER);
+		if(currentSongQuery!=null){
+			if(currentSongQuery.moveToPosition(mPosition)){
+				if(currentSongQuery.moveToNext()){
+					String song [] = new String[4];
+					song[ARTIST]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.ARTIST));
+					song[ALBUM]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.ALBUM));
+					song[TITLE]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.TITLE));
+					song[DATAPATH]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.DATA));
+					return song;	
+				} else {
+					return null;
+				}
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	}
+	/**
+	 *returns a string array with info about the current song
+	 * use PlaylistManager.ARTIST, PlaylistManager.ALBUM
+	 * PlaylistManager.TITLE, PlaylistManager.DATAPATH for the
+	 * indexes on the string array.
+	 * Returns null if there is no known current song
+	 * @return
+	 */
+	public String[] currentSong(){
+		Uri membersUri = MediaStore.Audio.Playlists.Members.getContentUri("external", mCurrentPlaylist);
+		Cursor currentSongQuery = cr.query(membersUri, STAR, null, null, Audio.Media.DEFAULT_SORT_ORDER);
+		if(currentSongQuery!=null){
+			if(currentSongQuery.moveToPosition(mPosition)){
+				String song [] = new String[4];
+				song[ARTIST]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.ARTIST));
+				song[ALBUM]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.ALBUM));
+				song[TITLE]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.TITLE));
+				song[DATAPATH]=currentSongQuery.getString(currentSongQuery.getColumnIndex(Audio.Media.DATA));
+				return song;	
+			}else {
+				return null;
+			}
+		} else {
+			return null;
+		}
+	} 
+
+
+
+	public void setPosition(int position) {
+		mPosition = position;	
+	}
+	public void setSelectedPlaylist(int playlistId){
+		mCurrentPlaylist = playlistId;
+	}
+
+
+
 }

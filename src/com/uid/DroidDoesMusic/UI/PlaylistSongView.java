176f387a142951ab88f532e4715eed4e544e1ed1
Joe Zeimen <jzeimen@gmail.com>
2010-11-21 19:47:15 -0700

    Adding PlaylistSongView

--- src/com/uid/DroidDoesMusic/UI/PlaylistSongView.java
+++ src/com/uid/DroidDoesMusic/UI/PlaylistSongView.java
@@ -26,7 +26,7 @@ import com.uid.DroidDoesMusic.util.PlaylistManager;
  * @author jzeimen
  *
  */
-public class Playlist extends ListActivity {
+public class PlaylistSongView extends ListActivity {
 	protected static final String TAG = "DroidDoesMusic";
 	public static final String INTENT_ITEM_PLAYLIST_NAME = "playlistName";
 	public static final String INTENT_ITEM_PLAYLIST_ID = "playlistID";
@@ -52,15 +52,8 @@ public class Playlist extends ListActivity {
         	playlistName = "";
         }            
 
-        
-        
-        if(playlistId != 0){
         	this.setListAdapter(PlaylistManager.getInstance(this).listSongs(playlistId));
-        } else {
-        	this.setListAdapter(PlaylistManager.getInstance(this).listPlaylists());
-        }
-        
-        
+                
     }
 	
 	@Override
@@ -71,7 +64,8 @@ public class Playlist extends ListActivity {
 		this.setListAdapter(PlaylistManager.getInstance(this).listSongs(position));
 		
 		
-		Intent i = new Intent(this,PlaylistSongView.class);
+		Intent i = new Intent(Intent.ACTION_PICK);
+		i.setDataAndType(Uri.EMPTY, "vnd.android.cursor.dir/ddm.track");
 		i.putExtra(Playlist.INTENT_ITEM_PLAYLIST_NAME,"Selected Playlist");
 		i.putExtra(Playlist.INTENT_ITEM_PLAYLIST_ID, (Integer)v.getTag(R.id.artist_id));
 		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -85,39 +79,3 @@ public class Playlist extends ListActivity {
 	
 }
 
-
-/*************Some Useful code I found on the web*****************
-final String [] STAR= {"*"};
-Log.i(TAG, "All the titles");
-Uri allaudio_uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-Cursor ca= managedQuery(allaudio_uri, STAR, null,null,null);
-for(ca.moveToFirst(); !ca.isAfterLast(); ca.moveToNext()){
-    if(ca.isFirst()){   // print all the fields of the first song
-        for(int k= 0; k<ca.getColumnCount(); k++)
-            Log.i(TAG, "  "+ca.getColumnName(k)+"="+ca.getString(k));
-    }else{              // but just the titles of the res
-        Log.i(TAG, ca.getString(ca.getColumnIndex("title")));
-    }
-}
-Log.i(TAG, "--------------------------");
-Log.i(TAG, "All the playlists");
-Uri playlist_uri= MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;    
-Cursor cursor= managedQuery(playlist_uri, STAR, null,null,null);
-cursor.moveToFirst();
-for(int r= 0; r<cursor.getCount(); r++, cursor.moveToNext()){
-    Log.i(TAG, "-----");
-    Log.i(TAG, "Playlist " + cursor.getString(cursor.getColumnIndex("name")));
-    for(int k= 0; k<cursor.getColumnCount(); k++)           
-        Log.i(TAG, cursor.getColumnName(k)+"="+cursor.getString(k));
-
-    // the members of this playlist
-    int id= cursor.getInt(0);
-    Uri membersUri = MediaStore.Audio.Playlists.Members.getContentUri("external", id);
-    Cursor membersCursor = managedQuery(membersUri, STAR, null, null, null);
-    membersCursor.moveToFirst();
-    for(int s= 0; s<membersCursor.getCount(); s++, membersCursor.moveToNext())
-        Log.i(TAG, "  "+membersCursor.getString(membersCursor.getColumnIndex("title")));
-    membersCursor.close();
-}
-cursor.close();
-/*******************************************************/

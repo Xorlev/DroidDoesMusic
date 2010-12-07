73b325b4bb515be8a810c546173d4c20b5cb1db5
Xorlev <elementation@gmail.com>
2010-12-06 19:53:05 -0700

    About version

--- src/com/uid/DroidDoesMusic/UI/About.java
+++ src/com/uid/DroidDoesMusic/UI/About.java
@@ -1,8 +1,12 @@
 package com.uid.DroidDoesMusic.UI;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.widget.TextView;
 
 import com.uid.DroidDoesMusic.R;
 
@@ -10,8 +14,24 @@ public class About extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        TextView tv;
+    	
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        
+        tv = (TextView)findViewById(R.id.about_version);
+        
+        ComponentName comp = new ComponentName(this, About.class);
+        PackageInfo pinfo;
+        
+		try {
+			pinfo = this.getPackageManager().getPackageInfo(comp.getPackageName(), 0);
+	        tv.setText("v" + pinfo.versionName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
     }
     
     @Override
